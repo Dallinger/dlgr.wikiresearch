364cd4edd5c46b3c07492881547dc669a678a992
Matthew Wilkes <git@matthewwilkes.name>
2017-08-31 16:58:30 +0100

    Add initial experiment.js functions

---
+++ dlgr/wikiresearch/static/scripts/experiment.js
@@ -0,0 +1,72 @@
+var my_node_id;
+
+// Consent to the experiment.
+$(document).ready(function() {
+
+    // do not allow user to close or reload
+    prevent_exit = true;
+
+    // Print the consent form.
+    $("#print-consent").click(function() {
+        console.log("hello");
+        window.print();
+    });
+
+    // Consent to the experiment.
+    $("#consent").click(function() {
+        store.set("hit_id", getUrlParameter("hit_id"));
+        store.set("worker_id", getUrlParameter("worker_id"));
+        store.set("assignment_id", getUrlParameter("assignment_id"));
+        store.set("mode", getUrlParameter("mode"));
+
+        allow_exit();
+        window.location.href = '/instructions';
+    });
+
+    // Consent to the experiment.
+    $("#no-consent").click(function() {
+        allow_exit();
+        window.close();
+    });
+
+    // Consent to the experiment.
+    $("#go-to-experiment").click(function() {
+        allow_exit();
+        window.location.href = '/exp';
+    });
+
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        submitResponses();
+    });
+
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        submitResponses();
+    });
+});
+
+// Create the agent.
+var create_agent = function() {
+    $('#finish-reading').prop('disabled', true);
+    reqwest({
+        url: "/node/" + participant_id,
+        method: 'post',
+        type: 'json',
+        success: function (resp) {
+            $('#finish-reading').prop('disabled', false);
+            my_node_id = resp.node.id;
+            get_info(my_node_id);
+        },
+        error: function (err) {
+            console.log(err);
+            errorResponse = JSON.parse(err.response);
+            if (errorResponse.hasOwnProperty('html')) {
+                $('body').html(errorResponse.html);
+            } else {
+                allow_exit();
+                go_to_page('questionnaire');
+            }
+        }
+    });
+};
